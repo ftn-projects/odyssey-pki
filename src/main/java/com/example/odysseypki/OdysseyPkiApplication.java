ca6e5c1d08884061f767207da25e6c5d2d7bce0e
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2024-04-19 10:19:15 +0200

    Add springboot application

---
+++ src/main/java/com/example/odysseypki/OdysseyPkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.odysseypki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdysseyPkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdysseyPkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/odysseypki/OdysseyPkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odysseypki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdysseyPkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
